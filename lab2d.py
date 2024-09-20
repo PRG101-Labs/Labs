0285814886fe5512f99619720d4a848279e90ffb
BushraBashirS <bushra.bashir25@gmail.com>
2024-09-20 12:35:20 -0400

    Update lab2d.py

--- lab2d.py
+++ lab2d.py
@@ -12,11 +12,4 @@
 
 # TO DO 2: copy the required lines from README.md to print argv[0], argv[1] and argv[2]
 # run the script using the following command: python lab2d.py maija Maija
-import sys
-print(sys.version) # prints the version of the python currently in use.
-print(sys.platform) # prints the name of operating system.
-#print(len(sys.argv)) # tells us the number of command line arguments the user provides from terminal.print(sys.argv) # prints the list of all arguments given at the command line when running our python script from terminal.
-print(len(sys.argv)) # tells us the number of command line arguments the user provides from terminal.
-print(sys.argv[0]) # prints the first argument, it is always the name of script.
-print(sys.argv[1]) # prints the second argument .
-print(sys.argv[2]) # prints the third argument.
+
