f19d20ab0a73f21d583e16ba4889f484374ba55d
BushraBashirS <bushra.bashir25@gmail.com>
2024-09-20 12:34:32 -0400

    Update lab2b.py

--- lab2b.py
+++ lab2b.py
@@ -8,13 +8,4 @@
 
 # TO DO 1:
 # Follow the instructions given in the README.md file.
-num = input("Please enter a 4-digit integer: ")
 
-# Check if the input is a valid 4-digit integer
-if num.isdigit() and len(num) == 4:
-    if int(num) == 1984:
-        print("George Orwell")
-    else:
-        print("Not quite right!")
-else:
-    print("Invalid input! Please enter a 4-digit integer.")
