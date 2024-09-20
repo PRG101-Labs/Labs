f4fb1377b73c849e6ec2f83448ead218462681e2
BushraBashirS <bushra.bashir25@gmail.com>
2024-09-20 12:34:14 -0400

    Update lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -7,18 +7,7 @@
 # Usage: ./lab2a.py
 
 # TO DO 1: Follow the instructions given in README.md file
-a=input("enter a number: ")
-#print(type(a))
-x= int(a)
-#print(type(x))
-#if x>=6:
- #   print("x is greater then 6!")
-#x is greater than or equal to 4 and x is less than 12.
 
 
-if x >= 4 and x < 12:
-    print("x is between 4 and 12 (inclusive of 4 and exclusive of 12).")
-else:
-    print("x is not in the range.")
 
 
