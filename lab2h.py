cd07adf48f4142a5fa3d948d84cbb2d1126bd3fd
BushraBashirS <bushra.bashir25@gmail.com>
2024-09-20 12:36:05 -0400

    Update lab2h.py

--- lab2h.py
+++ lab2h.py
@@ -11,13 +11,4 @@
 # The value of timer should initially be 10.
 # Use a while loop to create program that counts down from 10 with timer to 1.
 # When you reach 1 end the loop and print blast off!
-# Initialize the timer
-timer = 10
 
-# Countdown using a while loop
-while timer > 0:
-    print(timer)
-    timer -= 1  # Decrease the timer by 1
-
-# Print the final message
-print("Blast off!")
